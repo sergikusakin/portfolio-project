692569a816cf1556aab8730d2a5d519435cbf38f
Sergi <sergikusakin@gmail.com>
2023-11-23 13:09:26 +0100

    create base for pageFooter

---
+++ src/components/UI/pageFooter.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="flex justify-around content-center items-center text-white text-lg mt-5 h-40 bg-violet-500"
+  >
+    <div><router-link to="/">KUSAKINS DEV</router-link></div>
+    <div>Use technologies for you!</div>
+    <div>
+      <a href="https://www.linkedin.com/in/sergikusakin/"
+        >social: @sergikusakin</a
+      >
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "pageFooter",
+  data() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
